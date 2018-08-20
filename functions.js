840e2775e82e3e9a992f755455b773a0b32ef61d
osanchez <chebeto.sanchez@gmail.com>
2018-08-19 19:21:04 -0500

    Funciones

---
+++ functions.js
@@ -0,0 +1,17 @@
+// Funciones
+
+// Una funcion es un procedimiento repetitivo dentro del codigo, se emplean para tareas o calculos.
+
+// Una funcion se compone por la palabra clave FUNCTION seguida de su nombre CUADRADO, dentro de un parentesis se incluye un argumento que sera el que pase por la funcion. Dentro de los corchetes se usa el retorno que tendra dicha funcion, en este caso es un numero al cuadrado, el cual se calcula multiplicado por si mismo el numero ingresado.
+
+function cuadrado(numero) {
+  return  numero * numero;
+}
+
+console.log(cuadrado(6));
+
+
+var variable1 = "Sombreros";
+function compras() {
+  console.log(compras);
+}
