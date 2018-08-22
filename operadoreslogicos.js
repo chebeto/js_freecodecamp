c9daca1ec31fd14583d4a140a5309f324f038da3
osanchez <chebeto.sanchez@gmail.com>
2018-08-21 22:00:37 -0500

    && y || AND y OR

---
+++ operadoreslogicos.js
@@ -0,0 +1,13 @@
+// Operadores logicos
+
+// AND: &&    OR: ||
+
+var numero = 8
+
+if (numero > 5 && numero < 10) {
+  console.log("Estas dentro!"); // Se obtendria ESTAS DENTRO ya que el 8 es mayor que 5 y menor que 10.
+}
+
+if (numero > 5 || numero < 10) {
+  console.log("Estas dentro"); // Como se cumplen las dos condiciones, se imprime ESTAS DENTRO ya que cumple con las dos
+}
