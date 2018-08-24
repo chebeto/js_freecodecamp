a3a42d5afd203630daea437738f1cc3204f52126
osanchez <chebeto.sanchez@gmail.com>
2018-08-23 23:50:04 -0500

    2da parte de arreglos

--- arrays-methods.js
+++ arrays-methods.js
@@ -18,4 +18,14 @@ console.log(arr.concat(arr2)); // se obtendra ["a", "b", "c", "e", "f","g"]
 
 console.log(arr.join("")); // De esta forma podemos obtener el contenido del arreglo unido de la siguiente forma ABC
 
-console.log(arr.reverse()); // se imprime el arreglo al reves CBA 
+console.log(arr.reverse()); // se imprime el arreglo al reves CBA
+
+console.log(arr.shift()); // Elimina el primer articulo del arreglo
+
+console.log(arr.unshift("p")); // Inserta un nuevo articulo en la primer casilla del arreglo y devuelve el arreglo completo
+
+console.log(arr.slice(1,3)); // Imprime la seleccion del arreglo que se especifica en el metodo, por ejemplo, seleccionara solo la casilla 1 y 2, ya que la 3 no se incluye.
+
+console.log(arr.sort(arr)); // Ordena el arreglo alfabeticamete
+
+console.log(arr.splice(2,2, "NUEVO ELEMENTO DEL ARREGLO")); // En este caso, separa los elementos de
