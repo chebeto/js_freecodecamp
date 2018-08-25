17216656f059ffb874e961c9a8739e7590250241
osanchez <chebeto.sanchez@gmail.com>
2018-08-25 18:47:36 -0500

    While & Do While

---
+++ while_doWhile.js
@@ -0,0 +1,13 @@
+// While & Do while
+
+var n = 0;
+while (n < 5) {     // Mientras n sea menor que 5
+  n++;  // se suma 1 a n
+  console.log("n = " + n); // Se imprime n = 1 .. 2 .. 3 .. 4 ..
+}
+
+var i = 0;
+do {
+  i++; // Suma 1 a i
+  console.log("i = " + i); // imprime i
+} while (i < 5); // MIENTRAS i sea menor que 5
